5c71e21bc52f8d24b2f24c9ffc85b26f4f3d3ac5
Eliott <zefurtifpro@gmail.com>
2024-06-21 23:08:20 +0200

    EVA Added Body&Soul Movement

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-const SPEED = 80
+var PLAYING_BODY = true
+
+const BODY_SPEED = 80
+const SOUL_SPEED = 90
 const JUMP_VELOCITY = -300
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -9,6 +12,16 @@ func _ready():
 	pass
 
 func _process(delta):
+	
+	if Input.is_action_just_pressed("switch"):
+		PLAYING_BODY = !PLAYING_BODY
+	
+	if PLAYING_BODY:
+		handle_body_movement(delta)
+	else:
+		handle_soul_movement(delta)
+
+func handle_body_movement(delta):
 	if not $Body.is_on_floor():
 		$Body.velocity.y += gravity * delta
 	
@@ -17,12 +30,29 @@ func _process(delta):
 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		$Body.velocity.x = direction * SPEED
+		$Body.velocity.x = direction * BODY_SPEED
 		if $Body.velocity.x < 0:
 			$Body/AnimatedSprite2D.flip_h = true
 		if $Body.velocity.x > 0:
 			$Body/AnimatedSprite2D.flip_h = false
 	else:
-		$Body.velocity.x = move_toward($Body.velocity.x, 0, SPEED)
+		$Body.velocity.x = move_toward($Body.velocity.x, 0, BODY_SPEED)
 		
 	$Body.move_and_slide()
+
+func handle_soul_movement(delta):
+	var direction = Input.get_vector("move_left", "move_right","move_up", "move_down")
+	direction = direction.normalized()
+	if direction:
+		$Soul.velocity = direction * SOUL_SPEED
+		if $Soul.velocity.x < 0:
+			$Soul/AnimatedSprite2D.flip_h = true
+		if $Soul.velocity.x > 0:
+			$Soul/AnimatedSprite2D.flip_h = false
+	else:
+		$Soul.velocity.x = move_toward($Soul.velocity.x, 0, SOUL_SPEED)
+		$Soul.velocity.y = move_toward($Soul.velocity.y, 0, SOUL_SPEED)
+			
+	$Soul.move_and_slide()
+
+
