c328283d5b3524c3253d40a37f53418fa812a4c4
Eliott <zefurtifpro@gmail.com>
2024-06-23 06:33:10 +0200

    [EVA] HOTFIX

--- Scripts/DoorLevelA.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-	
-func _process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("interaction"):
-			get_tree().change_scene_to_file("res://Level/Hub/LevelA.tscn")
